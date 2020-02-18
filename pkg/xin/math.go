ca5983157ee6ff636848289f8a4c68a1be580d1f
Linus Lee <linus@thesephist.com>
2020-02-17 23:43:01 -0500

    Add math::[asin|acos|atan]

--- pkg/xin/math.go
+++ pkg/xin/math.go
@@ -81,6 +81,94 @@ func mathTanForm(fr *Frame, args []Value, node *astNode) (Value, InterpreterErro
 	}
 }
 
+func mathAsinForm(fr *Frame, args []Value, node *astNode) (Value, InterpreterError) {
+	if len(args) < 1 {
+		return nil, IncorrectNumberOfArgsError{
+			node:     node,
+			required: 1,
+			given:    len(args),
+		}
+	}
+
+	first := args[0]
+
+	switch cleanFirst := first.(type) {
+	case IntValue:
+		if cleanFirst > 1 || cleanFirst < -1 {
+			return zeroValue, nil
+		}
+
+		return FracValue(math.Asin(float64(cleanFirst))), nil
+	case FracValue:
+		if cleanFirst > 1 || cleanFirst < -1 {
+			return zeroValue, nil
+		}
+
+		return FracValue(math.Asin(float64(cleanFirst))), nil
+	}
+
+	return nil, MismatchedArgumentsError{
+		node: node,
+		args: args,
+	}
+}
+
+func mathAcosForm(fr *Frame, args []Value, node *astNode) (Value, InterpreterError) {
+	if len(args) < 1 {
+		return nil, IncorrectNumberOfArgsError{
+			node:     node,
+			required: 1,
+			given:    len(args),
+		}
+	}
+
+	first := args[0]
+
+	switch cleanFirst := first.(type) {
+	case IntValue:
+		if cleanFirst > 1 || cleanFirst < -1 {
+			return zeroValue, nil
+		}
+
+		return FracValue(math.Acos(float64(cleanFirst))), nil
+	case FracValue:
+		if cleanFirst > 1 || cleanFirst < -1 {
+			return zeroValue, nil
+		}
+
+		return FracValue(math.Acos(float64(cleanFirst))), nil
+	}
+
+	return nil, MismatchedArgumentsError{
+		node: node,
+		args: args,
+	}
+}
+
+func mathAtanForm(fr *Frame, args []Value, node *astNode) (Value, InterpreterError) {
+	if len(args) < 1 {
+		return nil, IncorrectNumberOfArgsError{
+			node:     node,
+			required: 1,
+			given:    len(args),
+		}
+	}
+
+	first := args[0]
+
+	switch cleanFirst := first.(type) {
+	case IntValue:
+		return FracValue(math.Atan(float64(cleanFirst))), nil
+	case FracValue:
+		return FracValue(math.Atan(float64(cleanFirst))), nil
+	}
+
+	return nil, MismatchedArgumentsError{
+		node: node,
+		args: args,
+	}
+}
+
 func mathLnForm(fr *Frame, args []Value, node *astNode) (Value, InterpreterError) {
 	if len(args) < 1 {
 		return nil, IncorrectNumberOfArgsError{
